94b4c514cf544153cd5ef89aca5298fd3c8ee445
Philip Peterson <im@starryexpanse.com>
2015-02-07 19:34:05 -0800

    Compatibility layer for use of wp-lightbox2 and Bootstrap, which conflict because of lightbox's use of Prototype.js

---
+++ inc/js/compat.js
@@ -0,0 +1,34 @@
+/*
+
+Code by evgeny.myasishchev
+From http://stackoverflow.com/questions/19139063/twitter-bootstrap-3-dropdown-menu-disappears-when-used-with-prototype-js
+
+licensed under cc by-sa 3.0 with attribution required
+
+*/
+
+(function() {
+    var isBootstrapEvent = false;
+    if (window.jQuery) {
+        var all = jQuery('*');
+        jQuery.each(['hide.bs.dropdown', 
+            'hide.bs.collapse', 
+            'hide.bs.modal', 
+            'hide.bs.tooltip',
+            'hide.bs.popover'], function(index, eventName) {
+            all.on(eventName, function( event ) {
+                isBootstrapEvent = true;
+            });
+        });
+    }
+    var originalHide = Element.hide;
+    Element.addMethods({
+        hide: function(element) {
+            if(isBootstrapEvent) {
+                isBootstrapEvent = false;
+                return element;
+            }
+            return originalHide(element);
+        }
+    });
+})();
